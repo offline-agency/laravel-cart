d64450ad5657343206c17cbc1ab6561896019281
Manuel Romanato <romanato.manuel@gmail.com>
2025-12-05 16:52:31 +0100

    no message

--- src/Exceptions/CartAlreadyStoredException.php
+++ src/Exceptions/CartAlreadyStoredException.php
@@ -4,4 +4,6 @@ namespace OfflineAgency\LaravelCart\Exceptions;
 
 use RuntimeException;
 
-class CartAlreadyStoredException extends RuntimeException {}
+class CartAlreadyStoredException extends RuntimeException
+{
+}
--- src/Exceptions/UnknownModelException.php
+++ src/Exceptions/UnknownModelException.php
@@ -4,4 +4,6 @@ namespace OfflineAgency\LaravelCart\Exceptions;
 
 use RuntimeException;
 
-class UnknownModelException extends RuntimeException {}
+class UnknownModelException extends RuntimeException
+{
+}
